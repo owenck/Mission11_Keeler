7e86b717c304955caae2fd1ee47fb9162ef44559
owenck <owenck@byu.edu>
2026-04-02 23:49:24 -0600

    I forgot an 's' and it broke everything :L

--- frontend/Mission11/src/api/BooksAPI.ts
+++ frontend/Mission11/src/api/BooksAPI.ts
@@ -1,6 +1,6 @@
 import type { Book } from '../types/Book';
 
-const API_BASE = 'https://mission13backend-dfg7chezb0g9dkdd.eastus-01.azurewebsites.net/api/books';
+const API_BASE = 'https://mission13backend-dfg7chezb0g9dkdd.eastus-01.azurewebsites.net/api/book';
 
 export async function fetchBooks(
     pageNum: number,
